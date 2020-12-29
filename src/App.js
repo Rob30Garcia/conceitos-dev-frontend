083877929d6166069c0c26b6e3a4650fb274311e
Robert <rgs2@icomp.ufam.edu.br>
2020-12-28 20:34:17 -0400

    feat: entendo o conceito de propriedades

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import { Header } from './components/Header';
 function App() {
   return (
     <>
-      <Header />
+      <Header title="Home">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
     </>
   );
 }
